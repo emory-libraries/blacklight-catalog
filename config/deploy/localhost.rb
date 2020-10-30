8add5e376ef1aac0c9334c13a8c7adec2b635147
bwatson78 <brad.watson.orlando@gmail.com>
2020-10-30 11:47:52 -0400

    corrects Gemfile after bundling and files related to Bixby errors.

--- config/deploy/localhost.rb
+++ config/deploy/localhost.rb
@@ -9,7 +9,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '127.0.0.1', user: 'deploy', roles: %w{app db web}
+server '127.0.0.1', user: 'deploy', roles: %w[app db web]
 
 # role-based syntax
 # ==================
