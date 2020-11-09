b091895c9d39f2d5075d27d73e30fa7c8dbf61a7
bwatson78 <brad.watson.orlando@gmail.com>
2020-11-09 14:13:26 -0500

    Implements Shibboleth into the application (#10).

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201028210217) do
+ActiveRecord::Schema.define(version: 20201109164154) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -48,8 +48,12 @@ ActiveRecord::Schema.define(version: 20201028210217) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "guest", default: false
+    t.string "provider"
+    t.string "uid"
+    t.string "display_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid"], name: "index_users_on_uid"
   end
 
 end
