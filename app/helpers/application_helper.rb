e991939b55c5cd168b02705c9ea33fea59c77f7c
devanshu-m <devanshu_m@gwmail.gwu.edu>
2021-02-03 13:14:00 -0500

    Adds initial work for request options

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # frozen_string_literal: true
 module ApplicationHelper
+  def openurl_base
+    "https://#{ENV['ALMA']}.alma.exlibrisgroup.com/view/uresolver/#{ENV['INSTITUTION']}/openurl?"
+  end
 end
