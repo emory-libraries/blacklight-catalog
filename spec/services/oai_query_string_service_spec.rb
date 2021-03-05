400960ce13a8b9f12bfa7718e0d61c0109cea0a7
Alex Zotov <alexandr.zotov@emory.edu>
2021-03-05 08:17:51 -0600

    modifies rake task to re-index a single record

--- spec/services/oai_query_string_service_spec.rb
+++ spec/services/oai_query_string_service_spec.rb
@@ -4,16 +4,27 @@ require 'rails_helper'
 RSpec.describe OaiQueryStringService, :clean do
   let(:oai_set) { 'blacklighttest' }
   let(:full_index) { false }
+  let(:single_record) { true }
   let(:to_time) { Time.new.utc.strftime("%Y-%m-%dT%H:%M:%SZ") }
   let(:ingest_time) { '2021-02-23T21:32:25Z' }
+  let(:institution) { ENV['INSTITUTION'] }
   PropertyBag.set('marc_ingest_resumption_token', '')
 
   before { PropertyBag.set('marc_ingest_time', ingest_time) }
   after { PropertyBag.delete_all }
 
   context '#process_query_string' do
+    it 'returns the right string when full_index false and resumption token not present and single_record true' do
+      qs = described_class.process_query_string(oai_set, full_index, to_time, single_record)
+
+      check_other_methods_called
+      expect(qs).to eq(
+        "?verb=GetRecord&identifier=oai:alma.#{institution}:#{oai_set}&metadataPrefix=marc21&until=#{to_time}&from=#{ingest_time}"
+      )
+    end
+
     it 'returns the right string when full_index false and resumption token not present' do
-      qs = described_class.process_query_string(oai_set, full_index, to_time)
+      qs = described_class.process_query_string(oai_set, full_index, to_time, false)
 
       check_other_methods_called
       expect(qs).to eq(
@@ -22,7 +33,7 @@ RSpec.describe OaiQueryStringService, :clean do
     end
 
     it 'returns the right string when full_index true and resumption token not present' do
-      qs = described_class.process_query_string(oai_set, true, to_time)
+      qs = described_class.process_query_string(oai_set, true, to_time, false)
 
       check_other_methods_called
       expect(qs).to eq(
@@ -32,7 +43,7 @@ RSpec.describe OaiQueryStringService, :clean do
 
     it 'returns the right string when resumption token present' do
       PropertyBag.set('marc_ingest_resumption_token', 'Hello!')
-      qs = described_class.process_query_string(oai_set, full_index, to_time)
+      qs = described_class.process_query_string(oai_set, full_index, to_time, false)
 
       check_other_methods_called
       expect(qs).to eq("?verb=ListRecords&resumptionToken=Hello!")
@@ -51,6 +62,6 @@ RSpec.describe OaiQueryStringService, :clean do
 
   def check_other_methods_called
     expect(described_class).to respond_to(:process_from_time).with(1).argument
-    expect(described_class).to respond_to(:process_string).with(4).argument
+    expect(described_class).to respond_to(:process_string).with(5).argument
   end
 end
