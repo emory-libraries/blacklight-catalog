c2c2f2dda50abb38e14338e4cffe69d76474b786
Brad Watson <brad.watson.orlando@gmail.com>
2021-03-12 12:17:10 -0500

    Fixes the redirects after login (#318). (#331)
    * Fixes the redirects after login (#318).
    
    * Minor tweak.
    
    * Another tweak.
    
    * Removes logging of omniauth.auth.
    
    * Removes the new session value.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def shibboleth
-    Rails.logger.debug "OmniauthCallbacksController#shibboleth: request.env['omniauth.auth']: #{request.env['omniauth.auth']}"
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      sign_in_and_redirect @user
       set_flash_message :notice, :success, kind: "Shibboleth"
+      sign_in @user
+      redirect_to request.env["omniauth.origin"] || root_path
     else
       redirect_to root_path
       set_flash_message(:notice, :failure, kind: "Shibboleth", reason: "you aren't authorized to use this application.")
