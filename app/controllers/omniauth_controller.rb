50aa3c5be3909dec2951bb2da755564f088f1d40
bwatson78 <brad.watson.orlando@gmail.com>
2021-03-15 09:41:41 -0400

    Oy vei!

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -3,7 +3,7 @@ class OmniauthController < Devise::SessionsController
   def new
     Rails.logger.debug "SessionsController#new: request.referer = #{request.referer}"
     if Rails.env.production?
-      session[:requested_page] = request.referer unless request.referer&.includes('sign_in')
+      session[:requested_page] = request.referer unless request.referer&.includes?('sign_in')
       redirect_to user_shibboleth_omniauth_authorize_path
     else
       super
