cb7761449193d236195398280b51dc114f718d4e
bwatson78 <brad.watson.orlando@gmail.com>
2021-03-15 08:36:15 -0400

    Further reworks redirects after login (#318).

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,11 +2,12 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def shibboleth
     @user = User.from_omniauth(request.env["omniauth.auth"])
-
+    Rails.logger.debug "OmniauthCallbacksController#shibboleth: session[:requested_page] = #{session[:requested_page]}"
+    Rails.logger.debug "OmniauthCallbacksController#shibboleth: request.env['omniauth.origin'] = #{request.env['omniauth.origin']}"
     if @user.persisted?
       set_flash_message :notice, :success, kind: "Shibboleth"
       sign_in @user
-      redirect_to request.env["omniauth.origin"] || root_path
+      redirect_to session[:requested_page] || request.env["omniauth.origin"] || root_path
     else
       redirect_to root_path
       set_flash_message(:notice, :failure, kind: "Shibboleth", reason: "you aren't authorized to use this application.")
--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 class OmniauthController < Devise::SessionsController
   def new
+    Rails.logger.debug "SessionsController#new: request.referer = #{request.referer}"
+    Rails.logger.debug "SessionsController#new: request.url = #{request.url}"
     if Rails.env.production?
+      session[:requested_page] = request.url
       redirect_to user_shibboleth_omniauth_authorize_path
     else
       super
