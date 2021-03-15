639793cb240f5c334adaf77443e0ba46692d1e3e
bwatson78 <brad.watson.orlando@gmail.com>
2021-03-15 09:36:28 -0400

    I think I've got it.

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -2,14 +2,9 @@
 class OmniauthController < Devise::SessionsController
   def new
     Rails.logger.debug "SessionsController#new: request.referer = #{request.referer}"
-    Rails.logger.debug "current_user here? #{current_user.present?}"
     if Rails.env.production?
-      session[:requested_page] = request.referer if current_user.blank?
-      if current_user.present?
-        redirect_to session[:requested_page] || root_path
-      else
-        redirect_to user_shibboleth_omniauth_authorize_path
-      end
+      session[:requested_page] = request.referer unless request.referer.includes('sign_in')
+      redirect_to user_shibboleth_omniauth_authorize_path
     else
       super
     end
