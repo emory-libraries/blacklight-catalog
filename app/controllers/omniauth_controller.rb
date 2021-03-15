1a5e918c8417577483c9c755de48238bfc722281
bwatson78 <brad.watson.orlando@gmail.com>
2021-03-15 09:50:57 -0400

    Please, let this be it.

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 class OmniauthController < Devise::SessionsController
   def new
-    Rails.logger.debug "SessionsController#new: request.referer = #{request.referer}"
     if Rails.env.production?
       session[:requested_page] = request.referer unless request.referer&.include?('sign_in')
       redirect_to user_shibboleth_omniauth_authorize_path
