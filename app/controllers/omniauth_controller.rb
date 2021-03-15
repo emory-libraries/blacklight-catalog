4d6a0f1fc0e27611ed53ddca1d5abddd082ed1b3
Brad Watson <brad.watson.orlando@gmail.com>
2021-03-15 10:11:33 -0400

    Further reworks redirects after login (#318). (#334)

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -2,6 +2,7 @@
 class OmniauthController < Devise::SessionsController
   def new
     if Rails.env.production?
+      session[:requested_page] = request.referer unless request.referer&.include?('sign_in')
       redirect_to user_shibboleth_omniauth_authorize_path
     else
       super
