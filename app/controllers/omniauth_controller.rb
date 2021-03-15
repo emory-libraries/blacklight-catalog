664d882f4d5006d7905d94e5dcc7ce60bcd5b238
bwatson78 <brad.watson.orlando@gmail.com>
2021-03-15 08:50:00 -0400

    Another tweak.

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -2,9 +2,8 @@
 class OmniauthController < Devise::SessionsController
   def new
     Rails.logger.debug "SessionsController#new: request.referer = #{request.referer}"
-    Rails.logger.debug "SessionsController#new: request.url = #{request.url}"
     if Rails.env.production?
-      session[:requested_page] = request.url
+      session[:requested_page] = request.referer
       redirect_to user_shibboleth_omniauth_authorize_path
     else
       super
