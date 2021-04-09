8dd2e128773cf4a265a3c8142769305b3e3d567a
McClain Looney <m@loonsoft.com>
2021-04-09 11:31:19 -0500

    Migrate arch2->arch

--- config/deploy/arch.rb
+++ config/deploy/arch.rb
@@ -55,8 +55,19 @@
 #     # password: "please use keys"
 #   }
 
+before 'deploy:check:linked_files', "deploy:copy_env"
+before 'deploy:symlink:linked_files', "deploy:copy_env"
+namespace :deploy do
+  task :copy_env do
+    on roles("web") do
+      upload!(".env.#{fetch(:stage).downcase}", "#{shared_path}/.env.production")
+      upload!("./config/secrets.yml", "#{shared_path}/config/secrets.yml")
+    end
+  end
+end
+
 set :stage, :ARCH
-set :ec2_region, %w[us-east-2]
+set :ec2_region, %w[us-east-1]
 ec2_role %i[web app db],
   user: 'deploy',
   ssh_options: {
