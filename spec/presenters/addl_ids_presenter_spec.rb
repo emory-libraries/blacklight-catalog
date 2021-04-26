7f51aca72fb8bda9d7c4fede860f7e07ae234445
bwatson78 <brad.watson.orlando@gmail.com>
2021-04-26 16:49:43 -0400

    Adjusts fields to register on Single View Page (#469).

--- spec/presenters/addl_ids_presenter_spec.rb
+++ spec/presenters/addl_ids_presenter_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe AddlIdsPresenter do
   let(:pres) { described_class.new(document: TEST_ITEM) }
   let(:details_terms) do
     { id: '123', isbn_ssim: ['SOME MAGICAL NUM .66G'], issn_ssim: ['SOME OTHER MAGICAL NUMBER .12Q'],
-      oclc_ssim: ['8675309'], other_standard_ids_ssim: ['M080142677'], publisher_number_ssim: ['H. 4260 H.'] }
+      oclc_ssim: ['8675309'], other_standard_ids_tesim: ['M080142677'], publisher_number_tesim: ['H. 4260 H.'] }
   end
   context 'with a solr document' do
     describe '#terms' do
--- spec/support/show_page_value.rb
+++ spec/support/show_page_value.rb
@@ -17,7 +17,7 @@ SHOW_PAGE_VALUE = {
     "title_abbr_tesim": ["Jenk. Story"], "title_translation_tesim": ["Le Stori de Jenkins"],
     "author_addl_display_tesim": ["Tim Jenkins"], "subject_display_ssim": ["Adventure"],
     "url_suppl_ssim": ["http://www.example.com"], "issn_ssim": ["SOME OTHER MAGICAL NUMBER .12Q"],
-    "oclc_ssim": ["8675309"], "other_standard_ids_ssim": ["M080142677"], "publisher_number_ssim": ["H. 4260 H."],
+    "oclc_ssim": ["8675309"], "other_standard_ids_tesim": ["M080142677"], "publisher_number_tesim": ["H. 4260 H."],
     "timestamp": "2021-03-18T14:43:52.592Z", "title_vern_display_tesim": ['Title of my Work']
   },
   field: "url_suppl_ssim"
