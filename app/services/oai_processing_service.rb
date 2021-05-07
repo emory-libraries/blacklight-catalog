97189acb7c2338b14cb6b8c7f21e4ff62974f2cb
bwatson78 <brad.watson.orlando@gmail.com>
2021-05-07 13:14:33 -0400

    Revises the Indexer to improve failure handling and logging (#502).

--- app/services/oai_processing_service.rb
+++ app/services/oai_processing_service.rb
@@ -5,76 +5,44 @@ require 'traject'
 
 class OaiProcessingService
   def self.process_oai_with_marc_indexer(institution, qs, alma, logger)
-    process_oai(institution, qs, alma, 'marc_indexer', logger)
-  end
-
-  def self.process_oai(institution, qs, alma, ingest_tool, logger)
-    oai_base = "https://#{alma}.alma.exlibrisgroup.com/view/oai/#{institution}/request"
-
-    log "Calling OAI with query string: #{qs}"
-    oai = RestClient.get oai_base + qs
-
-    document = Nokogiri::XML(oai)
-    marc_url = { 'marc' => "http://www.loc.gov/MARC21/slim" }
-
+    oai = call_oai_for_xml(alma, institution, qs, logger)
+    document = Nokogiri::XML(oai.body)
     # handling of delete records
-    deleted_records = document.xpath('/oai:OAI-PMH/oai:ListRecords/oai:record[oai:header/@status="deleted"]', { 'oai' => 'http://www.openarchives.org/OAI/2.0/' })
+    deleted_records = document.xpath('/oai:OAI-PMH/oai:ListRecords/oai:record[oai:header/@status="deleted"]', oai_url)
     suppressed_records = document.xpath("//marc:record[substring(marc:leader, 6, 1)='d']", marc_url) # gets all records with `d` in the 6th (actual) position of leader string
-    log "Found #{deleted_records.count + suppressed_records.count} delete records."
+    delete_suppressed_count = deleted_records.count + suppressed_records.count
+    logger.info "Found #{delete_suppressed_count} delete records."
+    deleted_ids = pull_deleted_ids(deleted_records, logger)
+    suppressed_ids = pull_suppressed_ids(suppressed_records, logger)
+    record_count = pull_record_count(document, suppressed_records, logger)
+    pull_active_ids(document, suppressed_ids, logger)
 
-    if (deleted_records.count + suppressed_records.count).positive?
-      deleted_ids = deleted_records.map { |n| n.at('header/identifier').text.split(':').last }
-      logger.announce_ids("Deleted")
-      logger.pile_on(deleted_ids)
-      suppressed_ids = suppressed_records.map { |s| s.at_xpath("marc:controlfield[@tag='001']", marc_url).text.to_i } # collects ID from controlfield 001
-      logger.announce_ids("Suppressed")
-      logger.pile_on(suppressed_ids)
-      deleted_ids += suppressed_ids
-      deleted_records.remove
-      suppressed_records.remove
-      puts RestClient.post "#{ENV['SOLR_URL']}/update?commit=true",
-                           "<delete><id>#{deleted_ids.join('</id><id>')}</id></delete>",
-                           content_type: :xml
-    end
+    deleted_records.remove
+    suppressed_records.remove
+    find_and_remove_del_supp_records(deleted_ids, suppressed_ids, logger) if delete_suppressed_count.positive?
 
     # Index remaining necessary records
-    record_count = document.xpath('/oai:OAI-PMH/oai:ListRecords/oai:record', { 'oai' => 'http://www.openarchives.org/OAI/2.0/' }).count - suppressed_records.count
-    log "#{record_count} records retrieved"
-    active_ids = document.xpath(
-      '/oai:OAI-PMH/oai:ListRecords/oai:record/oai:metadata/marc:record/marc:controlfield[@tag="001"]',
-      { 'oai' => 'http://www.openarchives.org/OAI/2.0/', 'marc' => "http://www.loc.gov/MARC21/slim" }
-    ).map { |v| v.content.to_i } - [*suppressed_ids]
-    logger.announce_ids('Active')
-    logger.pile_on(active_ids)
 
-    resumption_token = document.xpath('/oai:OAI-PMH/oai:ListRecords/oai:resumptionToken', { 'oai' => 'http://www.openarchives.org/OAI/2.0/' }).text
+    resumption_token = document.xpath('/oai:OAI-PMH/oai:ListRecords/oai:resumptionToken', oai_url).text
 
     if record_count.positive?
-      template = Nokogiri::XSLT(oai_to_marc)
-      file = Rails.root.join('tmp', resumption_token || 'last')
-      filename = "#{file}.xml"
-      File.open(filename, "w+") do |f|
-        f.write(template.transform(document).to_s)
+      begin
+        process_active_records_from_xml(resumption_token, document, logger)
+      rescue
+        return
       end
-
-      log "File written to tmp. Now indexing #{filename}"
-      case ingest_tool
-      when 'marc_indexer'
-        ingest_with_traject(filename)
-      end
-      File.delete(filename)
     end
 
     # return resumption token at the end by default
     resumption_token
   end
 
-  def self.ingest_with_traject(filename)
+  def self.ingest_with_traject(filename, logger)
     indexer = Traject::Indexer::MarcIndexer.new("solr_writer.commit_on_close": true)
     indexer.load_config_file(Rails.root.join('lib', 'marc_indexer.rb').to_s)
     indexer.process(filename)
   rescue => e
-    log e
+    logger.fatal e
   end
 
   def self.oai_to_marc
@@ -95,10 +63,69 @@ class OaiProcessingService
     )
   end
 
-  def self.log(msg)
-    time = Time.new.utc
-    time = time.strftime("%Y-%m-%d %H:%M:%S")
-    puts "#{time} - #{msg}"
-    true
+  def self.marc_url
+    { 'marc' => "http://www.loc.gov/MARC21/slim" }
+  end
+
+  def self.oai_url
+    { 'oai' => 'http://www.openarchives.org/OAI/2.0/' }
+  end
+
+  def self.find_and_remove_del_supp_records(deleted_ids, suppressed_ids, logger)
+    logger.info(solr.delete_by_id(deleted_ids + suppressed_ids).to_s)
+  end
+
+  def self.pull_deleted_ids(deleted_records, logger)
+    ids = deleted_records.map { |n| n.at('header/identifier').text.split(':').last }
+    logger.info "Deleted IDs: #{ids}"
+    ids
+  end
+
+  def self.pull_suppressed_ids(suppressed_records, logger)
+    # collects ID from controlfield 001
+    ids = suppressed_records.map { |s| s.at_xpath("marc:controlfield[@tag='001']", marc_url).text }
+    logger.info("Suppressed IDs: #{ids}")
+    ids
+  end
+
+  def self.pull_active_ids(document, suppressed_ids, logger)
+    ids = document.xpath(active_ids_xpath, oai_url.merge(marc_url)).map(&:content) - [*suppressed_ids]
+    logger.info "Active IDs: #{ids}"
+    ids
+  end
+
+  def self.active_ids_xpath
+    '/oai:OAI-PMH/oai:ListRecords/oai:record/oai:metadata/marc:record/marc:controlfield[@tag="001"]'
+  end
+
+  def self.solr
+    RSolr.connect(url: ENV['SOLR_URL'], update_format: :xml, retry_503: 5, retry_after_limit: 5)
+  end
+
+  def self.call_oai_for_xml(alma, institution, qs, logger)
+    oai_base = "https://#{alma}.alma.exlibrisgroup.com/view/oai/#{institution}/request"
+    oai_connection = Faraday.new do |f|
+      f.request :retry, { max: 10, interval: 30, interval_randomness: 0.75, backoff_factor: 2 }
+    end
+
+    logger.info "Calling OAI with query string: #{qs}"
+    oai_connection.get oai_base + qs
+  rescue => err
+    ["Communication with the OAI Service failed.", err].each { |m| logger.fatal(m) }
+  end
+
+  def self.pull_record_count(document, suppressed_records, logger)
+    record_ct = document.xpath('/oai:OAI-PMH/oai:ListRecords/oai:record', oai_url).count - suppressed_records.count
+    logger.info "#{record_ct} records retrieved"
+    record_ct
+  end
+
+  def self.process_active_records_from_xml(resumption_token, document, logger)
+    filename = Rails.root.join('tmp', "#{resumption_token || 'last'}.xml").to_s
+    File.open(filename, "w+") { |f| f.write(Nokogiri::XSLT(oai_to_marc).transform(document).to_s) }
+
+    logger.info "File written to tmp. Now indexing #{filename}"
+    ingest_with_traject(filename, logger)
+    File.delete(filename)
   end
 end
