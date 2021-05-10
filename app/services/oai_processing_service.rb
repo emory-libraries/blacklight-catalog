c64bbb53a3362b9a19de8636890ccd26c0dbdeab
bwatson78 <brad.watson.orlando@gmail.com>
2021-05-10 11:19:55 -0400

    Addresses the change requests made.

--- app/services/oai_processing_service.rb
+++ app/services/oai_processing_service.rb
@@ -4,12 +4,15 @@ require 'nokogiri'
 require 'traject'
 
 class OaiProcessingService
+  MARC_URL = { 'marc' => "http://www.loc.gov/MARC21/slim" }.freeze
+  OAI_URL = { 'oai' => 'http://www.openarchives.org/OAI/2.0/' }.freeze
+
   def self.process_oai_with_marc_indexer(institution, qs, alma, logger)
     oai = call_oai_for_xml(alma, institution, qs, logger)
     document = Nokogiri::XML(oai.body)
     # handling of delete records
-    deleted_records = document.xpath('/oai:OAI-PMH/oai:ListRecords/oai:record[oai:header/@status="deleted"]', oai_url)
-    suppressed_records = document.xpath("//marc:record[substring(marc:leader, 6, 1)='d']", marc_url) # gets all records with `d` in the 6th (actual) position of leader string
+    deleted_records = document.xpath('/oai:OAI-PMH/oai:ListRecords/oai:record[oai:header/@status="deleted"]', OAI_URL)
+    suppressed_records = document.xpath("//marc:record[substring(marc:leader, 6, 1)='d']", MARC_URL) # gets all records with `d` in the 6th (actual) position of leader string
     delete_suppressed_count = deleted_records.count + suppressed_records.count
     logger.info "Found #{delete_suppressed_count} delete records."
     deleted_ids = pull_deleted_ids(deleted_records, logger)
@@ -23,7 +26,7 @@ class OaiProcessingService
 
     # Index remaining necessary records
 
-    resumption_token = document.xpath('/oai:OAI-PMH/oai:ListRecords/oai:resumptionToken', oai_url).text
+    resumption_token = document.xpath('/oai:OAI-PMH/oai:ListRecords/oai:resumptionToken', OAI_URL).text
 
     if record_count.positive?
       begin
@@ -63,15 +66,8 @@ class OaiProcessingService
     )
   end
 
-  def self.marc_url
-    { 'marc' => "http://www.loc.gov/MARC21/slim" }
-  end
-
-  def self.oai_url
-    { 'oai' => 'http://www.openarchives.org/OAI/2.0/' }
-  end
-
   def self.find_and_remove_del_supp_records(deleted_ids, suppressed_ids, logger)
+    solr = RSolr.connect(url: ENV['SOLR_URL'], update_format: :xml, retry_503: 5, retry_after_limit: 5)
     logger.info(solr.delete_by_id(deleted_ids + suppressed_ids).to_s)
   end
 
@@ -83,23 +79,16 @@ class OaiProcessingService
 
   def self.pull_suppressed_ids(suppressed_records, logger)
     # collects ID from controlfield 001
-    ids = suppressed_records.map { |s| s.at_xpath("marc:controlfield[@tag='001']", marc_url).text }
+    ids = suppressed_records.map { |s| s.at_xpath("marc:controlfield[@tag='001']", MARC_URL).text }
     logger.info("Suppressed IDs: #{ids}")
     ids
   end
 
   def self.pull_active_ids(document, suppressed_ids, logger)
-    ids = document.xpath(active_ids_xpath, oai_url.merge(marc_url)).map(&:content) - [*suppressed_ids]
+    active_ids_xpath = '/oai:OAI-PMH/oai:ListRecords/oai:record/oai:metadata/marc:record/marc:controlfield[@tag="001"]'
+    ids = document.xpath(active_ids_xpath, OAI_URL.dup.merge(MARC_URL)).map(&:content) - [*suppressed_ids]
     logger.info "Active IDs: #{ids}"
     ids
-  end
-
-  def self.active_ids_xpath
-    '/oai:OAI-PMH/oai:ListRecords/oai:record/oai:metadata/marc:record/marc:controlfield[@tag="001"]'
-  end
-
-  def self.solr
-    RSolr.connect(url: ENV['SOLR_URL'], update_format: :xml, retry_503: 5, retry_after_limit: 5)
   end
 
   def self.call_oai_for_xml(alma, institution, qs, logger)
@@ -115,7 +104,7 @@ class OaiProcessingService
   end
 
   def self.pull_record_count(document, suppressed_records, logger)
-    record_ct = document.xpath('/oai:OAI-PMH/oai:ListRecords/oai:record', oai_url).count - suppressed_records.count
+    record_ct = document.xpath('/oai:OAI-PMH/oai:ListRecords/oai:record', OAI_URL).count - suppressed_records.count
     logger.info "#{record_ct} records retrieved"
     record_ct
   end
