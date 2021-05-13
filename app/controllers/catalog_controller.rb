cbdd08483d04cb6756dbd2ead0af4b76d1476aac
bwatson78 <brad.watson.orlando@gmail.com>
2021-05-13 10:26:31 -0400

    Removes Language.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -15,12 +15,12 @@ class CatalogController < ApplicationController
     config.advanced_search[:query_parser] ||= 'dismax'
     config.advanced_search[:form_solr_parameters] ||= {
       'facet.field' => [
-        "marc_resource_ssim", "library_ssim", "format_ssim", "language_ssim", "collection_ssim"
+        "marc_resource_ssim", "library_ssim", "format_ssim", "collection_ssim"
       ],
       "f.marc_resource_ssim.facet.limit" => -1,
       "f.library_ssim.facet.limit" => -1,
       "f.format_ssim.facet.limit" => -1,
-      "f.language_ssim.facet.limit" => -1,
+      "f.language_ssim.facet.limit" => 0,
       "f.collection_ssim.facet.limit" => -1
     }
     config.advanced_search[:form_facet_partial] ||= 'advanced_search_facets_as_select'
