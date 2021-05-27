9957305b082f255e28511495161273c18052ad34
Max Kadel <max@curationexperts.com>
2021-05-27 15:40:28 -0400

    test passing - returns json of uid, holding_id, and holding_library

--- app/services/alma_request_service.rb
+++ app/services/alma_request_service.rb
@@ -5,7 +5,36 @@ require 'nokogiri'
 class AlmaRequestService
   def initialize(mms_id, uid)
     @mms_id = mms_id
-    # @response = query_holding
-    # @xml = Nokogiri::XML(@response)
+    @response = query_holding
+    @xml = Nokogiri::XML(@response)
+    @json_response = json_response(uid)
+  end
+
+  def api_url
+    ENV['ALMA_API_URL'] || "https://api-na.hosted.exlibrisgroup.com"
+  end
+
+  def api_key
+    ENV.fetch('ALMA_BIB_KEY')
+  end
+
+  def query_holding
+    holdings_url = "#{api_url}/almaws/v1/bibs/#{@mms_id}/holdings?apikey=#{api_key}"
+    RestClient.get(holdings_url)
+  end
+
+  def holding_id
+    @xml.xpath("//holdings/holding/holding_id").text
+  end
+
+  def holding_library
+    @xml.xpath("//holdings/holding/library").text
+  end
+
+  def json_response(uid)
+    { uid: uid,
+      holding_id: holding_id,
+      holding_library: holding_library
+    }
   end
 end
--- spec/requests/request_requests_spec.rb
+++ spec/requests/request_requests_spec.rb
@@ -19,10 +19,14 @@ RSpec.describe 'Requests', type: :request, alma: true do
   it "can run a test" do
     # test object: 9937241630902486 Unwitting street
 
-    get requests_path, :params => { :request => { :mms_id => "9937241630902486" } }
+    get requests_path, :params => { :mms_id => "9937241630902486" }
     expect(response).to be_successful
     expect(response.body).to include(user.uid)
     expect(response.body).to include("22445410470002486") # Include holding ID for our test object
     expect(response.body).to include("UNIV") # Include holding Library for our test object
   end
 end
+
+@req_options = Nokogiri::XML(agent.get(
+  "https://#{host}/almaws/v1/bibs/#{mmsid}/request-options?user_id=#{@uid}&consider_dlr=false&apikey=#{apikey}"
+).body).xpath("//request_options/request_option/type[text()='HOLD']")
