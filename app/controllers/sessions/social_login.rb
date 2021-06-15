dd38531c0cf6544eef980ae5d0a4f962f4478c2b
Devanshu Matlawala <devanshu.matlawala@emory.edu>
2021-06-15 11:12:28 -0400

    Adds initial setup for social login via alma (#661)
    * We require affiliates to be able to login using their email via alma.
    * This commit initiates that work. We send a request for user login
    to alma's social login page and after they authenticate using a link
    in their email they are redirected back to the app with a jwt. Here,
    we decode the jwt and create or find user instance and the sign the user
    in using devise.

---
+++ app/controllers/sessions/social_login.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'jwt'
+
+module Sessions::SocialLogin
+  extend ActiveSupport::Concern
+
+  # User gets redirected here after a successful login
+  # via a social login provider.
+  def social_login_callback
+    begin
+      decoded_jwt = JWT.decode(params[:jwt], ENV["ALMA_AUTH_SECRET"], true, algorithm: "HS256")
+    rescue
+      flash[:error] = "there was a problem with the returned JSON web token"
+      redirect_to new_user_session_path and return
+    end
+    jwt = decoded_jwt[0]
+
+    user = social_login_user_model.find_or_initialize_by(provider: jwt["provider"], uid: jwt["id"])
+    user.assign_attributes(display_name: jwt["name"], email: jwt["email"])
+    user.save!
+
+    sign_in(:user, user)
+
+    social_login_populate_session(jwt)
+
+    redirect_to root_path
+  end
+
+  # populate the session
+  def social_login_populate_session(jwt)
+    session[:user_name] = jwt["name"]
+    session[:alma_id] = jwt["id"]
+    session[:alma_auth_type] = "social_login"
+    session[:alma_social_login_provider] = jwt["provider"]
+  end
+
+  # @return [Class] class object to use for users
+  def social_login_user_model
+    User
+  end
+end
---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe SessionsController, type: :controller do
+  let(:token) do
+    JWT.encode({ "iss" => "Alma", "aud" => "Primo", "exp" => (Time.current + 60).to_i,
+                 "jti" => "A_fFAWACiQDF6vml7IzzMQ", "iat" => 1_623_698_969,
+                 "nbf" => 1_623_698_849, "sub" => "patron", "id" => "bukowski1",
+                 "name" => "BUKOWSKI, CHARLES", "email" => "example@example.com",
+                 "provider" => "EMAIL" }, 'super_secret_key', 'HS256')
+  end
+  before do
+    request.env["devise.mapping"] = Devise.mappings[:user]
+  end
+
+  around do |example|
+    ENV['ALMA_AUTH_SECRET'] = 'super_secret_key'
+    example.run
+    ENV['ALMA_AUTH_SECRET'] = ''
+  end
+
+  context 'post call with bad params' do
+    it 'redirects to root_path with error flash message' do
+      post :social_login_callback, params: { jwt: 'foobar' }
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:error]).to be_present
+    end
+  end
+
+  context 'post call with good params' do
+    it 'redirects to root_path and signs user in' do
+      post :social_login_callback, params: { jwt: token }
+      expect(response).to redirect_to(root_path)
+      expect(flash[:error]).to be(nil)
+      expect(session[:alma_id]).to eq('bukowski1')
+      expect(User.first.uid).to eq('bukowski1')
+    end
+  end
+end
