1264784e33b3dac0c43967a3f388dc4d4d224457
McClain Looney <m@loonsoft.com>
2021-06-24 12:10:37 -0500

    Adds custom error pages for unhandled exceptions
    * removes unused templates
    * renamed static controller to errors controller
    * added handlers for codes 500 & 422

---
+++ spec/requests/errors_controller_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+require "rails_helper"
+# rubocop:disable RSpec/AnyInstance
+RSpec.describe ErrorsController, type: :request do
+  it "loads the page when 500 error detected" do
+    allow_any_instance_of(CatalogController).to receive(:show).and_raise("Whoa Beavis!")
+    get '/catalog/1'
+    expect(response.status).to eq 500
+    expect(response.body).not_to be_empty
+    expect(response.content_length).to be > 0
+    expect(response.content_type).to eq "text/html"
+    expect(response).to render_template(:unhandled_exception)
+  end
+  it "loads the page when 422 error detected" do
+    allow_any_instance_of(CatalogController).to receive(:show).and_raise(ActionController::InvalidAuthenticityToken)
+    get '/catalog/1'
+    expect(response.status).to eq 422
+    expect(response.body).not_to be_empty
+    expect(response.content_length).to be > 0
+    expect(response.content_type).to eq "text/html"
+    expect(response).to render_template(:unprocessable)
+  end
+
+  it "loads the page when 404 error detected" do
+    get '/catalog/1'
+    expect(response.status).to eq 404
+    expect(response.body).not_to be_empty
+    expect(response.content_length).to be > 0
+    expect(response.content_type).to eq "text/html"
+    expect(response).to render_template(:not_found)
+  end
+end
