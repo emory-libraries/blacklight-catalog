8f617685a2f3cbadebe36428bf6939eef3372a5e
Norm Orstad <norm@curationexperts.com>
2021-06-30 13:10:31 -0500

    Modal styles tightened. (#694)

--- app/helpers/citation_modal_helper.rb
+++ app/helpers/citation_modal_helper.rb
@@ -8,10 +8,10 @@ module CitationModalHelper
 
   def process_mult_documents_citations(documents)
     mla_cit_arr = documents.inject([]) do |arr, doc|
-      arr << tag.div(doc.send(:export_as_mla_citation_txt).html_safe, class: 'citation-text')
+      arr << tag.p(doc.send(:export_as_mla_citation_txt).html_safe, class: 'citation-text')
     end
     apa_cit_arr = documents.inject([]) do |arr, doc|
-      arr << tag.div(doc.send(:export_as_apa_citation_txt).html_safe, class: 'citation-text')
+      arr << tag.p(doc.send(:export_as_apa_citation_txt).html_safe, class: 'citation-text')
     end
 
     safe_join(
@@ -28,9 +28,9 @@ module CitationModalHelper
     safe_join(
       [
         tag.h2(t('blacklight.citation.mla'), class: 'citation-header'),
-        tag.div(documents.first.send(:export_as_mla_citation_txt).html_safe, class: 'citation-text'),
+        tag.p(documents.first.send(:export_as_mla_citation_txt).html_safe, class: 'citation-text'),
         tag.h2(t('blacklight.citation.apa'), class: 'citation-header'),
-        tag.div(documents.first.send(:export_as_apa_citation_txt).html_safe, class: 'citation-text')
+        tag.p(documents.first.send(:export_as_apa_citation_txt).html_safe, class: 'citation-text')
       ].flatten, ''
     )
   end
