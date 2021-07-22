5fd739e641a5b81a258961cd50a582089ef7d500
devanshu-m <devanshu_m@gwmail.gwu.edu>
2021-07-21 21:03:16 -0400

    Adds ability to save collections in a db table using qa
    * We are using Questioning Authority local database implementation
    to save collections in a database table. This will help render
    collection dropdown options faster on the advanced search page.

---
+++ config/initializers/advanced_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+# [Blacklight-Advanced-Search-v7.0.0-overwrite] Overwriting method index
+# for the AdvancedController to include dropdown menu options for
+# collection facet
+
+BlacklightAdvancedSearch::AdvancedController.class_eval do
+  def index
+    @response = get_advanced_search_facets unless request.method == :post
+    collection_auth = Qa::LocalAuthority.find_by(name: 'collections')
+    @collection_ssim_facet = Qa::LocalAuthorityEntry.where(local_authority_id: collection_auth)&.pluck(:uri) || []
+  end
+end
