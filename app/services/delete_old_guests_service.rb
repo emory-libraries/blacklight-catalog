b141be45df08e4252e0256485865921e3a476622
Devanshu Matlawala <devanshu.matlawala@emory.edu>
2021-07-22 16:12:36 -0400

    Adds logging when deleting guest users and associations (#776)

--- app/services/delete_old_guests_service.rb
+++ app/services/delete_old_guests_service.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 class DeleteOldGuestsService
   def self.destroy_users
+    log_file = Rails.env.production? ? "log/production.log" : "log/development.log"
+    Rails.logger = Logger.new(log_file)
+    Rails.logger.info "Deleting all Guest users along with their bookmarks"
     User.where(["created_at < ? AND uid LIKE 'guest_%'", 30.minutes.ago]).destroy_all
   end
 end
