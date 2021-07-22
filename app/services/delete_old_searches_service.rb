70aff080d3f65efd54e4950049f90732429baf89
devanshu-m <devanshu_m@gwmail.gwu.edu>
2021-07-22 14:52:26 -0400

    Adds logging when deleting guest users and associations

--- app/services/delete_old_searches_service.rb
+++ app/services/delete_old_searches_service.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 class DeleteOldSearchesService
   def self.destroy_searches
+    log_file = Rails.env.production? ? "log/production.log" : "log/development.log"
+    Rails.logger = Logger.new(log_file)
+    Rails.logger.info "Deleting all Searches where User is NULL"
     Search.where(["created_at < ? AND user_id IS NULL", 30.minutes.ago]).destroy_all
   end
 end
