0bf34770d9545b767c3617617cda0e2e6f00fb26
devanshu-m <devanshu_m@gwmail.gwu.edu>
2021-07-30 15:00:24 -0400

    Attempts to improve col filter even more
    * We will now wait for user to type at least three letters before we
    begin searching. We are also limiting number of results returned
    in an attempt to not freeze the search. Finally, we are only looking
    for options where typed letters are beginning of words.

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class CollectionsController < ApplicationController
   def search
-    @collection_result = Qa::LocalAuthorityEntry.where("uri LIKE lower(?)", "%#{params[:uri].downcase}%").pluck(:uri) || []
+    @collection_result = Qa::LocalAuthorityEntry.where("uri LIKE lower(?)", "#{params[:uri].downcase}%").limit(10)&.pluck(:uri) || []
     render json: { collection_result: @collection_result }
   end
 end
