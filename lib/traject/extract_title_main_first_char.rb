1b4b054c47719512c2e8d2cd75140006dbfe7902
bwatson78 <brad.watson.orlando@gmail.com>
2021-08-06 15:02:25 -0400

    Adds foreign languages.

--- lib/traject/extract_title_main_first_char.rb
+++ lib/traject/extract_title_main_first_char.rb
@@ -6,7 +6,7 @@ module ExtractTitleMainFirstChar
   def extract_title_main_first_char
     lambda do |rec, acc|
       titles = marc21.extract_marc_from(rec, '245abfgknps', alternate_script: false)
-      titles.each { |t| acc << t.match(/\w/)[0].upcase } if titles.present?
+      titles.each { |t| acc << t.match(/(\w|\p{L})/)[0].upcase } if titles.present?
     end
   end
 end
