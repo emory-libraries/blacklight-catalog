5a4524c66b89b10514b104e164dbcbea05c922af
bwatson78 <brad.watson.orlando@gmail.com>
2021-08-27 12:57:50 -0400

    Converts the Title Starts With facet to a navigation menu (#851).

--- app/helpers/search_results_helper.rb
+++ app/helpers/search_results_helper.rb
@@ -8,4 +8,23 @@ module SearchResultsHelper
     return safe_join(vern_titles, tag('br')) if vern_titles.present?
     ''
   end
+
+  def first_char_search_state(search_state)
+    search_state.to_h.merge('sort': 'title_ssort asc, pub_date_isim desc')
+  end
+
+  def first_char_active_letter(state)
+    state['f']['title_main_first_char_ssim']&.first if state['f']
+  end
+
+  def first_char_letter_hash(state, letter)
+    letter_state = state.dup
+    letter_state['f'] = { 'title_main_first_char_ssim': [letter] }
+    letter_state
+  end
+
+  def first_char_cleared_hash(state)
+    state['f']&.delete('title_main_first_char_ssim')
+    state
+  end
 end
