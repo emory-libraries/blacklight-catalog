264dc97949e008f3567ffdc3453e5ef391dc61fe
Ayoub Belemlih <13107510+abelemlih@users.noreply.github.com>
2021-10-06 11:39:11 -0400

    Set root path as requested page when referer is the sign in page

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,12 +4,12 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      set_flash_message :notice, :success, kind: "Shibboleth"
+      set_flash_message :notice, :success, kind: "Emory"
       sign_in @user
       redirect_to session[:requested_page] || request.env["omniauth.origin"] || root_path
     else
       redirect_to root_path
-      set_flash_message(:notice, :failure, kind: "Shibboleth", reason: "you aren't authorized to use this application.")
+      set_flash_message(:notice, :failure, kind: "Emory", reason: "you aren't authorized to use this application.")
     end
   end
 end
