08d0557f4f15a91e7cf8a6234bb67568a990c548
bwatson78 <brad.watson.orlando@gmail.com>
2021-11-01 11:56:16 -0400

    Fixes formatting.

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -7,6 +7,7 @@ module CatalogHelper
     t('blacklight.application_name',
         default: t('blacklight.application_name', locale: I18n.default_locale))
   end
+
   def generic_solr_value_to_url(value)
     url_arr = build_arr_links_text_split(values_of_field(value))
     return safe_join(url_arr, tag('br')) if url_arr.present?
