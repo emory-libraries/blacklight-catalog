c4c2643989c47e3275d0c3f57625751fc13d3e5c
bwatson78 <brad.watson.orlando@gmail.com>
2021-11-22 09:35:33 -0500

    Updates ebook verbiage (#1061).

--- spec/system/front_page_spec.rb
+++ spec/system/front_page_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe 'front page', type: :system do
   end
 
   it 'has expected text' do
-    expect(page).to have_css('h3.header-search', text: 'Search books, eBooks, journals, videos and more')
+    expect(page).to have_css('h3.header-search', text: 'Search books, e-books, journals, videos and more')
   end
 
   it('does not have the Title Starts With nav') { expect(page).not_to have_css('.first-main-char-ol') }
