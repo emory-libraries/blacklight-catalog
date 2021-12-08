80409df0d1883cb2bb1032bd28c1a312dbd75204
Ayoub Belemlih <13107510+abelemlih@users.noreply.github.com>
2021-12-08 17:02:59 -0500

    Add contact us availability indicator

--- app/controllers/availability_indicators_controller.rb
+++ app/controllers/availability_indicators_controller.rb
@@ -8,7 +8,7 @@ class AvailabilityIndicatorsController < ApplicationController
     documents = search_service.fetch(document_ids, rows: document_ids.count).first.documents
     documents.each do |document|
       availability = availability_data.present? ? availability_data[document.id] : nil
-      indicator = render_to_string('catalog/_availability_indicator', layout: false, locals: { xray: false, document: document, doc_avail_values: availability })
+      indicator = render_to_string('catalog/_availability_indicator', layout: false, locals: { document: document, doc_avail_values: availability })
       availability_indicators[document.id] = indicator
     end
     render json: availability_indicators
