c109819bf7a1e86541301c05b5192dd40a20bbd4
Ayoub Belemlih <13107510+abelemlih@users.noreply.github.com>
2023-03-09 18:36:40 -0600

    Resolve show page issues

--- app/presenters/related_names_presenter.rb
+++ app/presenters/related_names_presenter.rb
@@ -3,13 +3,21 @@
 class RelatedNamesPresenter
   attr_reader :document, :config
 
-  def initialize(document:)
-    @document = document
-    @config = YAML.safe_load(File.open(Rails.root.join('config', 'metadata', 'related_names.yml')))
+  def initialize(fields:)
+    @config = YAML.safe_load(File.open(Rails.root.join('config', 'metadata', 'related_names.yml'))).symbolize_keys
+    @fields = fields
   end
 
   def terms
-    @config = @config.symbolize_keys
-    @document.slice(*@config.keys)
+    filter(fields: @fields, keys: @config.keys)
+  end
+
+  private
+
+  def filter(fields:, keys:)
+    fields.select do |field_name, _, _|
+      name = field_name.to_sym
+      keys.include? name
+    end
   end
 end
