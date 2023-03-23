975006b3b06e990f83a3a3a11a9be2941bf361dd
Ayoub Belemlih <ayoub.belemlih@emory.edu>
2023-03-23 10:59:01 -0500

    Add additional titles components (#1334)

---
+++ app/components/metadata_component.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class MetadataComponent < Blacklight::DocumentMetadataComponent
+  # @param config [String]
+  # @param fields [Enumerable<Blacklight::FieldPresenter>]
+  # @param title [String]
+  # @param show [Boolean]
+  def initialize(config: nil, fields: [], title: '', show: false)
+    @title = title
+    keys = config.present? ? YAML.safe_load(File.open(Rails.root.join('config', 'metadata', config))) : []
+    fields = filter(fields: fields, keys: keys)
+    super(fields: fields, show: show)
+  end
+
+  # @param field Blacklight::FieldPresenter
+  def field_component(field)
+    field&.component || MetadataFieldComponent
+  end
+
+  private
+
+  # @param fields [Enumerable<Blacklight::FieldPresenter>]
+  # @param included [String]
+  def filter(fields:, keys:)
+    return fields if keys.blank?
+
+    fields.select do |field|
+      keys.include? field.key
+    end
+  end
+end
