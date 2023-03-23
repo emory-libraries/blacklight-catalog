d1ee35d34f964598b10595d62442cf4180ecbba2
Ayoub Belemlih <13107510+abelemlih@users.noreply.github.com>
2023-03-23 12:38:11 -0500

    Use view components to display remaining show page fields

--- app/presenters/description_summary_presenter.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class DescriptionSummaryPresenter < FieldPresenter
-  CONFIG_PATH = Rails.root.join('config', 'metadata', 'description_summary.yml')
-
-  def initialize(fields:)
-    super(path: CONFIG_PATH, fields: fields)
-  end
-end
