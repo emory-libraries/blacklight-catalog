bb24a0d49269707a0d792011635725a48da0a079
Ayoub Belemlih <ayoub.belemlih@emory.edu>
2023-03-23 12:23:47 -0500

    Display related names metadata using view components (#1336)

--- app/presenters/related_names_presenter.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class RelatedNamesPresenter < FieldPresenter
-  CONFIG_PATH = Rails.root.join('config', 'metadata', 'related_names.yml')
-
-  def initialize(fields:)
-    super(path: CONFIG_PATH, fields: fields)
-  end
-end
