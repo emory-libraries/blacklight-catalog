52dce6a4b2ec3c906ef2e1ac529742ada5e7b712
Ayoub Belemlih <13107510+abelemlih@users.noreply.github.com>
2023-03-23 12:38:11 -0500

    Use view components to display remaining show page fields

--- app/presenters/subjects_genre_presenter.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class SubjectsGenrePresenter < FieldPresenter
-  CONFIG_PATH = Rails.root.join('config', 'metadata', 'subjects_genre.yml')
-
-  def initialize(fields:)
-    super(path: CONFIG_PATH, fields: fields)
-  end
-end
