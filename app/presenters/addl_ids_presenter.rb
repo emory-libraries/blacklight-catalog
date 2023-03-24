6ac829d049c3d02bcbe9ddde2f5626fcfc85c76e
Ayoub Belemlih <ayoub.belemlih@emory.edu>
2023-03-24 11:07:09 -0500

    Use view components to display remaining fields on show page (#1338)

--- app/presenters/addl_ids_presenter.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class AddlIdsPresenter < FieldPresenter
-  CONFIG_PATH = Rails.root.join('config', 'metadata', 'addl_ids.yml')
-
-  def initialize(fields:)
-    super(path: CONFIG_PATH, fields: fields)
-  end
-end
