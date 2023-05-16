777c83bf10c40dcbcc5959d45719db00f11e9a61
Ayoub Belemlih <13107510+abelemlih@users.noreply.github.com>
2023-05-16 18:09:23 -0500

    Improve guest user deletion performance

--- app/services/delete_old_guests_service.rb
+++ app/services/delete_old_guests_service.rb
@@ -3,9 +3,13 @@ class DeleteOldGuestsService
   def self.destroy_users
     log_file = Rails.env.production? ? "log/production.log" : "log/development.log"
     Rails.logger = Logger.new(log_file)
+    # Whenever we upgrade Blacklight, we need to ensure all data associated to guests is deleted properly
+    # whenever guest users are deleted. For now, only bookmarks and searches are associated with guest users are deleted.
     start = DateTime.now
     Rails.logger.info "Deleting all Guest users along with their bookmarks"
-    User.where(["created_at < ? AND uid LIKE 'guest_%'", 1.day.ago]).destroy_all
+    Bookmark.where(["user_id IN (SELECT id FROM `users` WHERE (created_at < ? AND uid LIKE 'guest_%') )", 1.day.ago]).delete_all
+    Search.where(["user_id IN (SELECT id FROM `users` WHERE (created_at < ? AND uid LIKE 'guest_%') )", 1.day.ago]).delete_all
+    User.where(["created_at < ? AND uid LIKE 'guest_%'", 1.day.ago]).delete_all
     Rails.logger.info "Guest deletion started at #{start} and finished at #{DateTime.now}."
   end
 end
