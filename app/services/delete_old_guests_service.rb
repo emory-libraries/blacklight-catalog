4b9dfb6c36161cd6474874921d746f2c61579e60
Ayoub Belemlih <13107510+abelemlih@users.noreply.github.com>
2023-05-16 18:10:40 -0500

    Update delete_old_guests_service.rb

--- app/services/delete_old_guests_service.rb
+++ app/services/delete_old_guests_service.rb
@@ -4,7 +4,7 @@ class DeleteOldGuestsService
     log_file = Rails.env.production? ? "log/production.log" : "log/development.log"
     Rails.logger = Logger.new(log_file)
     # Whenever we upgrade Blacklight, we need to ensure all data associated to guests is deleted properly
-    # whenever guest users are deleted. For now, only bookmarks and searches are associated with guest users are deleted.
+    # whenever guest users are deleted. For now, only bookmarks and searches are deleted.
     start = DateTime.now
     Rails.logger.info "Deleting all Guest users along with their bookmarks"
     Bookmark.where(["user_id IN (SELECT id FROM `users` WHERE (created_at < ? AND uid LIKE 'guest_%') )", 1.day.ago]).delete_all
