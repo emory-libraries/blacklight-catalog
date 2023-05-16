323f8d4032f1c1ff4fc5f5df695c3a3878e94935
Collin Brittle <cnbrittle@gmail.com>
2023-05-15 20:57:16 -0400

    Delete guest users and searches once a day
    Previously, this ran every 30 minutes, and did not seem to complete in a
    reliable way. This should attempt to run less frequently, and report
    start and end times when it finishes.

--- app/services/delete_old_guests_service.rb
+++ app/services/delete_old_guests_service.rb
@@ -3,7 +3,9 @@ class DeleteOldGuestsService
   def self.destroy_users
     log_file = Rails.env.production? ? "log/production.log" : "log/development.log"
     Rails.logger = Logger.new(log_file)
+    start = DateTime.now
     Rails.logger.info "Deleting all Guest users along with their bookmarks"
-    User.where(["created_at < ? AND uid LIKE 'guest_%'", 30.minutes.ago]).destroy_all
+    User.where(["created_at < ? AND uid LIKE 'guest_%'", 1.day.ago]).destroy_all
+    Rails.logger.info "Guest deletion started at #{start} and finished at #{DateTime.now}."
   end
 end
--- app/services/delete_old_searches_service.rb
+++ app/services/delete_old_searches_service.rb
@@ -3,7 +3,9 @@ class DeleteOldSearchesService
   def self.destroy_searches
     log_file = Rails.env.production? ? "log/production.log" : "log/development.log"
     Rails.logger = Logger.new(log_file)
+    start = DateTime.now
     Rails.logger.info "Deleting all Searches where User is NULL"
-    Search.where(["created_at < ? AND user_id IS NULL", 30.minutes.ago]).destroy_all
+    Search.where(["created_at < ? AND user_id IS NULL", 1.day.ago]).destroy_all
+    Rails.logger.info "Search deletion started at #{start} and finished at #{DateTime.now}."
   end
 end
