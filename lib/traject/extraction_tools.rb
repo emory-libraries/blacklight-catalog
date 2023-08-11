60269dcbffb2f5362651fbcecbf5491d4bce9af2
Ayoub Belemlih <13107510+abelemlih@users.noreply.github.com>
2023-08-11 15:06:47 -0500

    Update extraction_tools.rb

--- lib/traject/extraction_tools.rb
+++ lib/traject/extraction_tools.rb
@@ -175,16 +175,6 @@ module ExtractionTools
     field_values.empty? ? nil : field_values.join(separator)
   end
 
-  def subject_tesim_str(atoz)
-    %W[
-      600#{atoz}:610#{atoz}:611#{atoz}:630#{atoz}:650#{atoz}
-      651#{atoz}:653#{atoz}:654#{atoz}:655#{atoz}:656#{atoz}
-      657#{atoz}:658#{atoz}:662#{atoz}:688#{atoz}:690#{atoz}
-      691#{atoz}:692#{atoz}:693#{atoz}:694#{atoz}:695#{atoz}
-      696#{atoz}:697#{atoz}:698#{atoz}:699#{atoz}
-    ].join(':').freeze
-  end
-
   def title_series_str(atog)
     %W[
       440anpv:490av
