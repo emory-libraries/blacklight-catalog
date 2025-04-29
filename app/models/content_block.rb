cfb61f1e542a400094932d8d7b9a781e97740ab7
azotov <alexandr.zotov@emory.edu>
2025-04-29 14:55:41 -0500

    adds ability to add a tags to banner (#1449)

--- app/models/content_block.rb
+++ app/models/content_block.rb
@@ -2,6 +2,7 @@
 class ContentBlock < ApplicationRecord
   validates :reference, presence: true
   delegate :blank?, to: :value
+  before_save :sanitize_value
 
   def self.homepage_banner
     ContentBlock.find_by(reference: 'homepage_banner') || ContentBlock.blank(reference: 'homepage_banner')
@@ -9,5 +10,17 @@ class ContentBlock < ApplicationRecord
 
   def self.blank(reference:)
     ContentBlock.new(reference:, value: '')
+  end
+
+  private
+
+  def sanitize_value
+    return unless value.present? && value_changed?
+
+    # Allow only 'a' tags.
+    allowed_tags = %w[a]
+    # Allow specific attributes for <a> tags.
+    allowed_attributes = %w[href target title class id]
+    self.value = ActionController::Base.helpers.sanitize(value, tags: allowed_tags, attributes: allowed_attributes)
   end
 end
